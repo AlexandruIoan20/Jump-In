de5a0d15d3c6bc9b98341f0c9f0337351f7aeb2a
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2024-12-14 15:26:35 +0200

    setup for project fixed

---
+++ project1/global/global.cpp
@@ -0,0 +1,5 @@
+#include "global.h"
+#include <iostream>
+using namespace std;
+
+int var = 31;
---
+++ project1/global/global.h
@@ -0,0 +1,10 @@
+//
+// Created by ialex on 14.12.2024.
+//
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern int var;
+
+#endif //GLOBAL_H
